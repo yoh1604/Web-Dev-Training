1edb13115b859762c41e865234b6650c90222683
yoh1604 <115641112+yoh1604@users.noreply.github.com>
2023-01-30 01:05:42 +0700

    first commit ⚒️

---
+++ HTML/html-css-assignment-3-v3/tailwind.config.cjs
@@ -0,0 +1,9 @@
+//  File to enable tailwindcss intellisense
+module.exports = {
+    // content: ["./src/index.css.{html,js}"],
+    content: ["./src/index.html.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
